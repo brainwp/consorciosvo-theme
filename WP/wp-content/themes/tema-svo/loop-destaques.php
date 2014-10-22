93f61e42892abc8ff6366423a929736cef69186c
Bruno Magrini <brmagrini@gmail.com>
2014-10-22 15:14:58 -0200

    organiza noticias em destaque e comum, com estilo e ajax

--- WP/wp-content/themes/tema-svo/loop-destaques.php
+++ WP/wp-content/themes/tema-svo/loop-destaques.php
@@ -2,40 +2,62 @@
     <?php
 $counter = 1;
 $destaques_query_args = array( 'post_type' => 'post', 'meta_key' =>'destaque', 'meta_value' => 's',
-  'posts_per_page'=>'2','paged'=>$paged);
+  'posts_per_page'=>'3','paged'=>$paged);
 $destaques_query = new WP_Query();
 $destaques_query->query($destaques_query_args);
 ?>
 
 <?php while ( $destaques_query->have_posts() ) : $destaques_query->the_post(); ?>
-    <div class="noticia inline-block <?php switch ($counter) {
-	  case 1:
-	    echo 'col-md-6';
-	    break;
-	  case 2:
-	    echo 'col-md-4';
-	    break;
-	}?>" id="<?php echo 'noticia-destaque'.$counter;?>">
-		<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h2><?php the_title(); ?></h2></a>
-		<div class="post-thumbnail"><?php if ( has_post_thumbnail() ) {the_post_thumbnail();}?>
-			<div class="data">
-				<?php echo get_the_date()?>
-			</div>
-		</div><!--post-thumbnail-->
-		
-		<div class="resumo ">
-		 <?php the_excerpt(); ?> 
-		</div><!--resumo-->
-	</div> <!-- noticia-->
-	<?php $mod=$counter % 2;
-				if ($mod==0) {
-			echo "<div class='clearfix'></div>";
-			}
-			$counter++;?>
-  <?php endwhile; ?>
+   <?php switch ($counter) {
+	  	case 1:?>
+			 <div class="noticia inline-block col-md-7" id="<?php echo 'noticia-destaque'.$counter;?>">
+				<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h2><?php the_title(); ?></h2></a>
+				<div class="post-thumbnail"><?php if ( has_post_thumbnail() ) {the_post_thumbnail();}?>
+					<?php the_excerpt(); ?> 
+
+				</div><!--post-thumbnail-->
+				<div class="data">
+					<?php echo get_the_date()?>
+				</div>
+			</div> <!-- noticia-->
+	
+		<?php 
+		break;
+		case 2:?>
+			 <div class="noticia inline-block col-md-5" id="<?php echo 'noticia-destaque'.$counter;?>">
+	    		
+				<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h3><?php the_title(); ?></h3></a>
+				<div class="post-thumbnail col-md-6"><?php if ( has_post_thumbnail() ) {the_post_thumbnail();}?>
+				</div><!--post-thumbnail-->
+				<div class="data">
+					<?php echo get_the_date()?>
+				</div>
+				<div class="post-excerpt"><?php the_excerpt(); ?></div>
+				
+			</div> <!-- noticia-->
+	
+		<?php
+   		break;
+   		case 3:?>
+			 <div class="noticia inline-block col-md-5" id="<?php echo 'noticia-destaque'.$counter;?>">
+	    		
+				<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h2><?php the_title(); ?></h2></a>
+				<div class="post-excerpt"><?php the_excerpt(); ?></div>
+				<div class="data">
+					<?php echo get_the_date()?>
+				</div>
+
+			</div> <!-- noticia-->
+	
+		<?php
+       	break;
+		}
+		$counter++;
+	 	endwhile; ?>
+	<div class='clearfix'></div>
     <div id="pagination">
-    <?php next_posts_link('Destaques Antigos', $destaques_query->max_num_pages) ?>
-    <?php previous_posts_link('Destaques Rececentes') ?>
+    	<?php next_posts_link('Destaques Antigos', $destaques_query->max_num_pages) ?>
+    	<?php previous_posts_link('Destaques Rececentes') ?>
     </div>
 </div><!-- #destaques-noticias -->
 
--- WP/wp-content/themes/tema-svo/loop-noticia.php
+++ WP/wp-content/themes/tema-svo/loop-noticia.php
@@ -3,42 +3,32 @@
 $counter = 1;
 $noticias_query_args = array( 'post_type' => 'post', 'meta_key' => 'destaque',
 	'meta_value' => 'n',
-  'posts_per_page'=>'2','paged'=>$paged);
+  'posts_per_page'=>'4','paged'=>$paged);
 $noticias_query = new WP_Query();
 $noticias_query->query($noticias_query_args);
 ?>
 
 <?php while ( $noticias_query->have_posts() ) : $noticias_query->the_post(); ?>
-    <div class="noticia inline-block <?php switch ($counter) {
-	  case 1:
-	    echo 'col-md-6';
-	    break;
-	  case 2:
-	    echo 'col-md-4';
-	    break;
-	}?>" id="<?php echo 'noticia-'.$counter;?>">
-		<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h2><?php the_title(); ?></h2></a>
-		<div class="post-thumbnail"><?php if ( has_post_thumbnail() ) {the_post_thumbnail();}?>
+    <div class="noticia inline-block col-md-3" id="<?php echo 'noticia-'.$counter;?>">
+		<a class="titulo" href="<?php echo post_permalink($post->ID); ?>"><h3><?php the_title(); ?></h3></a>
 			<div class="data">
 				<?php echo get_the_date()?>
 			</div>
-		</div><!--post-thumbnail-->
 		
 		<div class="resumo ">
-		 <?php the_excerpt(); ?> 
+		 	<?php the_excerpt(); ?> 
 		</div><!--resumo-->
 	</div> <!-- noticia-->
-	<?php $mod=$counter % 2;
-				if ($mod==0) {
-			echo "<div class='clearfix'></div>";
-			}
+	<?php 
 			$counter++;?>
   <?php endwhile; ?>
+	<div class='clearfix'></div>
+
     <div id="pagination2">
-    <?php next_posts_link('Destaques Antigos', $noticias_query->max_num_pages) ?>
-    <?php previous_posts_link('Destaques Rececentes') ?>
+    	<?php next_posts_link('Notícias Antigos', $noticias_query->max_num_pages) ?>
+    	<?php previous_posts_link('Notícias Rececentes') ?>
     </div>
-</div><!-- #noticias -->
+</div><!-- #noticias-ajax -->
 
 <?php wp_reset_postdata();
 ?>
